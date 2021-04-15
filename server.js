b2e2a7a714c01217e0a4e09791cabc1be6f2e3ab
SRRahmaaaan <sydurrahman2699@gmai.com>
2021-04-16 02:20:11 +0600

    payment done

--- server.js
+++ server.js
@@ -2,12 +2,15 @@
 const express = require("express")
 const cors = require("cors")
 const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectId
 require("dotenv").config()
+const stripe = require("stripe")(`${process.env.STRIPE_KEY}`);
+const { v4: uuidv4 } = require("uuid");
 const app = express()
 // =================================================MIDDLEWARE=======================================
 app.use(cors())
 app.use(express.json())
-app.use(express.urlencoded({extended: false}))
+app.use(express.urlencoded({extended: true}))
 const port = process.env.PORT || 8000
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.se8dl.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -18,6 +21,7 @@ app.get("/", (req, res) => {
 client.connect(err => {
     const servicesCollection = client.db("servicesDb").collection("services");
     const adminCollection = client.db("adminDb").collection("admins");
+    const ordersCollection = client.db("ordersDb").collection("orders");
 
     app.post("/addService", (req, res) => {
         const newService = req.body
@@ -33,18 +37,22 @@ client.connect(err => {
             res.send(documents)
         })
     })
-    
 
+    app.get("/specific/:id", (req, res) => {
+        const id = req.params.id
+        servicesCollection.find({ _id: ObjectId(id) })
+        .toArray((errors, documents) => {
+            res.send(documents[0])
+        })
+    })
 
-
-
-
-
-
-
-
-
-
+    app.post("/customerOrder", (req, res) => {
+        const newOrder = req.body
+        ordersCollection.insertOne(newOrder)
+        .then(result => {
+            res.send(result.insertedCount > 0)
+        })
+    })
 
     app.post("/addAdmin", (req, res) => {
         const newAdmin = req.body
@@ -55,8 +63,28 @@ client.connect(err => {
     })
 
 });
-
-
+//===================================================PAYMENT ROUTE===================================
+app.post("/payment", (req, res) => {
+    const { service, token } = req.body
+    const idempotencyKey = uuidv4
+    return stripe.customers.create({
+        email: token.email,
+        source: token.source
+    })
+    .then(customer => {
+        stripe.charges.create({
+            amount: service.price * 100,
+            currency: "usd",
+            customer: customer.id,
+            shipping: {
+                name: token.card.name,
+                address: token.card.address_country
+            }
+        }, {idempotencyKey})
+    })
+    .then(result => res.status(200).json(result))
+    .catch(error => console.log(error))
+})
 
 
 
